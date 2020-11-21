edb6d84ae10ad91e6ec7347ab45304fa6572fe04
ReffSeven <74449710+ReffSeven@users.noreply.github.com>
2020-11-21 21:35:48 +0700

    Create matrix-angka-2-1loop.py

---
+++ angka/matrix-angka-2-1loop.py
@@ -0,0 +1,17 @@
+"""
+1 2 3 4 5 6 7 
+2 3 4 5 6 7 6 
+3 4 5 6 7 6 5 
+4 5 6 7 6 5 4 
+5 6 7 6 5 4 3 
+6 7 6 5 4 3 2 
+7 6 5 4 3 2 1 
+"""
+N = 7; Nd = 1; Nr = 0;
+for y in range(1, N*N+1):
+	Np = (Nd + Nr)
+	Na = Np if(Np<=N) else(N-((N-Np)*-1))
+	print(Na, end=" ")
+
+	if(y%N==0): Nd = 1; Nr += 1; print("")
+	else: Nd += 1
